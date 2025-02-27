4d971b871fe261e5d466f6e1687e221445d4cff7
Kaifolow <alex6v1998@gmail.com>
2025-02-27 23:07:15 +0700

    удалил билдовую

--- build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_FileVieWer_FILE E:/programming/FileViewer/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/FileVieWer.exe)
-set(__QT_DEPLOY_TARGET_FileVieWer_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
